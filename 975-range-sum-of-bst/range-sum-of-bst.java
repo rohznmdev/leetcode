9b919f10926b6c120051fdd1af5a1fe30dad0cb6
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-10-31 15:54:14 -0400

    Time: 0 ms (100.00%) | Memory: 50.9 MB (75.97%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -22,9 +22,7 @@ class Solution {
     }
     private void dfs(TreeNode node, int low, int high) {
         if (node != null) {
-            if (node.val >= low && node.val <= high) {
-                ans += node.val; 
-            }
+            if (node.val >= low && node.val <= high) ans += node.val; 
             if (low < node.val) dfs(node.left, low, high);
             if (node.val < high) dfs(node.right, low, high); 
         }
