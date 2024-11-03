89d5c56b0b7b6cb73ea3cb0a9d9578b654ee58d5
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-02 21:38:02 -0400

    Time: 11 ms (97.71%) | Memory: 45.9 MB (13.06%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,27 +1,27 @@
 class Solution {
-    private static final int[][] DIRECTIONS = new int[][]{
+    private int[][] DIRECTIONS = {
         {-1, -1}, {-1, 0}, {-1, 1}, 
-        {0, -1}, {0, 1}, 
+        {0, -1}, {0, 1},
         {1, -1}, {1, 0}, {1, 1}
-    };
+    }; 
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length; 
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1;
         Queue<int[]> q = new LinkedList<>(); 
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) return -1; 
-        int[] start = new int[]{0, 0, 1}; 
-        q.offer(start); 
+        // [row, col, dist]
+        q.offer(new int[]{0, 0, 1}); 
         grid[0][0] = 1; 
         while (!q.isEmpty()) {
             int[] cell = q.poll(); 
             int row = cell[0]; 
             int col = cell[1]; 
-            int distance = cell[2]; 
-            if (row == n - 1 && col == n - 1) return distance; 
-            for (int[] dir : DIRECTIONS) {
-                int newRow = row + dir[0]; 
-                int newCol = col + dir[1]; 
+            int dist = cell[2]; 
+            if (row == n - 1 && col == n - 1) return dist; 
+            for (int[] direction : DIRECTIONS) {
+                int newRow = row + direction[0]; 
+                int newCol = col + direction[1]; 
                 if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && grid[newRow][newCol] == 0) {
-                    q.offer(new int[]{newRow, newCol, distance + 1}); 
+                    q.offer(new int[]{newRow, newCol, dist + 1}); 
                     grid[newRow][newCol] = 1; 
                 }
             }
