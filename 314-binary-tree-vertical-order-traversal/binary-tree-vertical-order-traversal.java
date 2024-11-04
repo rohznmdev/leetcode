25a4c0f7199d42b8e43ab59d97e701cd1fdf3b10
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-04 13:18:55 -0500

    Time: 4 ms (20.64%) | Memory: 42.9 MB (35.28%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        Map<Integer, List<Integer>> map = new TreeMap<>(); 
+        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>(); 
+        if (root != null) q.offer(new Pair<>(root, 0)); 
+        while (!q.isEmpty()) {
+            Pair<TreeNode, Integer> curr = q.poll(); 
+            TreeNode node = curr.getKey(); 
+            int col = curr.getValue(); 
+            map.putIfAbsent(col, new ArrayList<>()); 
+            map.get(col).add(node.val); 
+            if (node.left != null) q.offer(new Pair<>(node.left, col - 1)); 
+            if (node.right != null) q.offer(new Pair<>(node.right, col + 1)); 
+        }
+        return new ArrayList<>(map.values()); 
+    }
+}
