8ca0752c4d7d8773ac86efeeffe3b20ca9033b10
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-04 11:19:37 -0500

    Time: 0 ms (100.00%) | Memory: 45.9 MB (14.61%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,16 +2,17 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0; 
         int right = nums.length - 1; 
-        while (left <= right) {
-            int middle = left + ((right - left) / 2); 
-            if (nums[middle] > target) {
-                right = middle - 1; 
-            } else if (nums[middle] < target) {
+        while (left < right) {
+            int middle = left + (right - left) / 2; 
+            // we know that the nums[middle] < target so we can skip past everything from the left to the middle + 1 as we know target is not in that portion of the array. 
+            if (nums[middle] < target) {
                 left = middle + 1; 
             } else {
-                return middle; 
+                // we know that nums[middle] > target so we need to exclude everything from the right side to the middle as it is bigger and target won't be in there. 
+                right = middle; 
             }
         }
-        return -1; 
+        if (nums[left] != target) return -1; 
+        return left;
     }
 }
