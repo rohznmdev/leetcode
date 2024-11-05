66e99aaa2239eb9c82555ddeb4d954a87b3dd1d9
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-05 10:46:12 -0500

    Time: 23 ms (98.38%) | Memory: 50 MB (26.49%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.java
+++ 912-random-pick-with-weight/random-pick-with-weight.java
@@ -7,6 +7,9 @@ class Solution {
         cumulativeWeights = new int[w.length];
         totalWeight = 0; 
         random = new Random(); 
+        // 1, 2, 3
+        // 1, 3, 6
+        // [1], [2, 3], [4, 5, 6]
         for (int i = 0; i < w.length; i++) {
             totalWeight += w[i]; 
             cumulativeWeights[i] = totalWeight; 
@@ -14,11 +17,13 @@ class Solution {
     }
     
     public int pickIndex() {
-        int target = random.nextInt(0, totalWeight) + 1; 
+        int target = random.nextInt(totalWeight) + 1; 
         int left = 0; 
         int right = cumulativeWeights.length - 1; 
+        
         while (left < right) {
             int middle = left + (right - left) / 2; 
+            // if cumulativeWeights[middle] < target, we know the target lies in the right half and that the middle can be excluded.
             if (cumulativeWeights[middle] < target) {
                 left = middle + 1; 
             } else {
