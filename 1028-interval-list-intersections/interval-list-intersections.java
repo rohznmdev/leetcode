7d54d75b6c160cc2fcf68419408408834ba4610b
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-05 10:23:13 -0500

    Time: 3 ms (98.67%) | Memory: 45.3 MB (89.78%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -4,11 +4,13 @@ class Solution {
         int i = 0; 
         int j = 0; 
         while (i < firstList.length && j < secondList.length) {
+            // intersection is the larger of the starting intervals
+            // and smaller of the ending intervals
             int start = Math.max(firstList[i][0], secondList[j][0]); 
             int end = Math.min(firstList[i][1], secondList[j][1]); 
-            if (start <= end) {
-                sol.add(new int[]{start, end}); 
-            }
+            // check if its an intersection 
+            if (start <= end) sol.add(new int[]{start, end}); 
+            // move forward in the list whose interval ends earlier
             if (firstList[i][1] < secondList[j][1]) {
                 i++; 
             } else {
