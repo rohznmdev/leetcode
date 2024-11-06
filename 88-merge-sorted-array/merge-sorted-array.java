571b7cb1417c0a6e4aa2ebe7aa13c3533c5e7c73
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-05 22:24:57 -0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (98.82%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,22 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums1Pointer = m - 1;
-        int nums2Pointer = n - 1; 
-        int sortPointer = nums1.length - 1; 
-        while (nums1Pointer >= 0 && nums2Pointer >= 0) {
-            if (nums1[nums1Pointer] < nums2[nums2Pointer]) {
-                nums1[sortPointer] = nums2[nums2Pointer]; 
+        int n1Pointer = m - 1; 
+        int n2Pointer = n - 1; 
+        int sortPointer = nums1.length -1; 
+        while (n1Pointer >= 0 && n2Pointer >= 0) {
+            if (nums1[n1Pointer] < nums2[n2Pointer]) {
+                nums1[sortPointer] = nums2[n2Pointer]; 
                 sortPointer--; 
-                nums2Pointer--; 
+                n2Pointer--; 
             } else {
-                nums1[sortPointer] = nums1[nums1Pointer]; 
-                nums1Pointer--; 
+                nums1[sortPointer] = nums1[n1Pointer]; 
                 sortPointer--; 
+                n1Pointer--;
             }
         }
-        while (nums2Pointer >= 0) {
-            nums1[sortPointer] = nums2[nums2Pointer]; 
-            nums2Pointer--; 
+        // handles any remaining elements from nums2 that haven't been copied over
+        // don't need to check cuz we know nums2 is sorted so we can just put them in nums1 at the next sort pointer position 
+        // because nums1 has the length of both nums1 and nums2 there's a possibility that there's nothing in nums1 but something in nums2 that needs to be copied. this will take care of that.  
+        while (n2Pointer >= 0) {
+            nums1[sortPointer] = nums2[n2Pointer]; 
+            n2Pointer--; 
             sortPointer--; 
         }
     }
