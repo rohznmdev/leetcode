b36e51e3173f52610ed399f84ab4b32046a57531
Rohan Mathew <74740240+rohznmdev@users.noreply.github.com>
2024-11-05 20:08:44 -0500

    Time: 4 ms (99.64%) | Memory: 45.4 MB (28.85%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,23 +1,29 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int left = 0; 
-        int right = s.length() - 1;
+        int right = s.length() - 1; 
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
-                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1); 
+                /**
+                if they are not equal we need to check if there is a substring that when removing a character forms a palinfrome
+                left + 1 means we are removing the character at left by moving + 1 and keeping right the same
+                right - 1 means we are removing the character at right by moving - 1 and keeping left the same
+                we use or because we only need one of these modified strings to return true for us to have our answer
+                 */
+                return checkPalindrome(s, left + 1, right) || checkPalindrome(s, left, right - 1); 
             }
             left++; 
             right--; 
-        } 
+        }   
         return true; 
-        
     }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) return false; 
-            left++; 
-            right--; 
+    private boolean checkPalindrome(String s, int low, int high) {
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high)) {
+                return false;
+            }
+            low++; 
+            high--; 
         }
         return true; 
     }
